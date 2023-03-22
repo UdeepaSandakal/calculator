811ff592c5adf11a3b34d49362c7206d0df400b2
UdeepaSandakal <2017asp02@vau.jfn.ac.lk>
2023-03-22 18:54:19 +0530

    creating a number pad

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -29,6 +29,27 @@ class _CalculatorState extends State<Calculator> {
         title: Text('Calculator'),
         backgroundColor: Color(0xff0a0321),
       ),
-      
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 5),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    '0',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(color: Colors.white, fontSize: 80),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
